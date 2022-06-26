2bd887dfc80b46d442ab5613c1bf43147457545d
Luchabarreau <lulubarreau3@gmail.com>
2022-06-26 16:45:24 -0300

    cambios

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -193,6 +193,9 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  var str = String (numero).length
+  if (str === 3)
+  return true
   
 }
 
@@ -200,6 +203,14 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var contador = 0
+  var suma = numero
+  do {
+    suma = suma + 5
+    contador++
+    
+  } while (contador < 8);
+  return suma
 }
 
 
