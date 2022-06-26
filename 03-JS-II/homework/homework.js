2f0a2a9ef3f409e3372917d10dd975e2ee239f10
Luchabarreau <lulubarreau3@gmail.com>
2022-06-26 16:56:38 -0300

    cambios

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -160,11 +160,11 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
 
-  if (numero <2) return false
-  for (let i = 2; i < numero; i++){
+  if(numero <2) return false
+  for(let i=2; i < numero; i++){
     if (numero % 1 === 0) return false
   }
-  
+
   return true
 
 }
