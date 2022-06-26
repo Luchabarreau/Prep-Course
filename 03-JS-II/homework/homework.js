e2f92e49f49b3dd880bb36faa31863266361942e
Luchabarreau <lulubarreau3@gmail.com>
2022-06-26 13:30:25 -0300

    cambios

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -147,7 +147,7 @@ function operadoresLogicos(num1, num2, num3) {
     return "Número 1 es mayor y positivo"
   }
   if (num3 > num1 || num3 > num2){
-    return num3 + 1
+    return num3 + 1;
   }else{
     return false
   }
