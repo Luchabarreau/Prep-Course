91536aa0ebd636a8887979feb23671387871e77a
Luchabarreau <lulubarreau3@gmail.com>
2022-06-26 12:25:47 -0300

    npm test JSII.test.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -66,7 +66,22 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch (color) {
+    case "blue":
+      return "This is blue"
+      break;
+    case "red":
+      return "This is red";
+    case "green":
+      return "This is green";
+    case "orange":
+      return "This is orange"
   
+    default:
+      return "Color not found"
+      break;
+  }
+
 }
 
 function esDiezOCinco(numero) {
