7b95f2dec3626b698fb990743fd7fb97fbfe60fe
Luchabarreau <lulubarreau3@gmail.com>
2022-06-27 20:36:59 -0300

    cambios

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -157,6 +157,10 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  let num = n.toString();
+  let num1 = num.split("");
+  if(num1[0] === "9") return true
+  return false;
   
 }
 
