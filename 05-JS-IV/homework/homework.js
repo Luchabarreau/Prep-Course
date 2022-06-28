91fdd9bed470cfdbe853256b7da68f66e87c26d3
Luchabarreau <lulubarreau3@gmail.com>
2022-06-28 18:06:00 -0300

    cambios

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -89,7 +89,7 @@ function verificarPassword (usuario, password) {
   // De lo contrario, devuelve "false"
   // Tu código:
   return usuario[password] === password
-  return false
+
 }
 
 function actualizarPassword (usuario, nuevaPassword) {
