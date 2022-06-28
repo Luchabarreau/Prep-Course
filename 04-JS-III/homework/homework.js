735ea0b7de4898efd42f1cdfd98db80121798b21
Luchabarreau <lulubarreau3@gmail.com>
2022-06-27 21:42:46 -0300

    cambios

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -246,6 +246,13 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  let array3 = []
+  for(let i = 0; i < 10; i++){
+    if(i===5)continue;
+    numero = numero + 2
+    array3.push(numero);
+  }
+  return array3
 }
 
 
